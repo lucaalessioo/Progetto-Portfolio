73bbc447e7511ac4247fc98ed199bd5c1526180d
lucaalessioo <lucaalessioo@hotmail.com>
2026-05-12 14:35:25 +0200

    Applicati test sul metodo saveWork della classe Work

--- src/test/java/com/makeup/portfolio/service/WorksServiceTest.java
+++ src/test/java/com/makeup/portfolio/service/WorksServiceTest.java
@@ -1,5 +1,16 @@
 package com.makeup.portfolio.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,6 +18,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
 
 import com.makeup.portfolio.DTO.WorksDTO;
 import com.makeup.portfolio.mapper.WorksMapper;
@@ -43,8 +55,10 @@ public class WorksServiceTest {
         work = new Work();
         work.setId(1L);
         work.setTitle("Trucco sposa");
+        work.setImageUrl("/uploads/test.jpg");
 
         worksDto = new WorksDTO();
+        worksDto.setId(1L);
         worksDto.setTitle("Trucco Sposa");
 
     }
@@ -57,10 +71,29 @@ public class WorksServiceTest {
     @Test
     void testGetAllWorks() {
 
+        
+
     }
 
     @Test
-    void testSaveWork() {
+    void testSaveWork() throws IOException {
+
+        //GIVEN
+        MockMultipartFile file = new MockMultipartFile(
+            "file","test.jpg","image/jpeg","contenuto immagine".getBytes());
+
+            when(categoryRepository.findById(1L)).thenReturn(Optional.of(category));
+            when(workRepository.save(any(Work.class))).thenReturn(work);
+            when(worksMapper.toDto(any(Work.class))).thenReturn(worksDto);
+
+        //WHEN
+        WorksDTO result = worksService.saveWork("Titolo", "Descrizione",1L, file);
+
+        //THEN
+        assertNotNull(result);
+        assertEquals("Trucco Sposa", result.getTitle());
+        verify(workRepository, times(1)).save(any(Work.class));
+        verify(categoryRepository, times(1)).findById(1L);
 
     }
 
